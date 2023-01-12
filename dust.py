d44e68e0cc2f7bf1e16807b053615e7eb0aaa60c
Shin JH <shinjh4903@gmail.com>
2023-01-12 17:30:08 +0900

    파이썬 기초

---
+++ dust.py
@@ -0,0 +1,8 @@
+dust = int(input())
+
+if dust > 50:
+    print("50초과")
+
+else:
+    print("50이하")
+
---
+++ for_loop.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4]
+
+# numbers 리스트가 가진 모든 요소를 순화 할 때 까지
+# numbers가 가진 각 요소를 출력
+
+for i in numbers:
+    print(i)
+
